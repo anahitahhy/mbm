b2c6d8723c25357169f117642ea6645539328bb6
Anahita Hayati <134133439+anahitahhy@users.noreply.github.com>
2024-11-29 14:36:59 +0330

    Update tmn.py

--- tmn.py
+++ tmn.py
@@ -1,41 +1,39 @@
 import numpy as np
-import matplotlib.pyplot as plt
+import cv2
 
-
+# تابعی برای ایجاد گرادیان رنگ بین دو رنگ
 def interpolate_color(color1, color2, t):
     return (1 - t) * np.array(color1) + t * np.array(color2)
 
-
+# تابعی برای رسم تصویر با گرادیان رنگ
 def draw_gradient(point1, point2, color1, color2):
     width, height = 400, 400
     image = np.zeros((height, width, 3))
 
-    
+    # محاسبه شیب خط
     dx = point2[0] - point1[0]
     dy = point2[1] - point1[1]
     length = np.sqrt(dx**2 + dy**2)
 
     for y in range(height):
         for x in range(width):
-        
+            # محاسبه فاصله از نقطه اول
             d1 = np.sqrt((x - point1[0])**2 + (y - point1[1])**2)
             d2 = np.sqrt((x - point2[0])**2 + (y - point2[1])**2)
 
-        
+            # محاسبه t برای رنگ
             if d1 + d2 != 0:
                 t = d1 / (d1 + d2)
                 image[y, x] = interpolate_color(color1, color2, t)
 
     return image
 
-
-point1 = (200,100 )  
-point2 = (100, 300)  
-color1 = (0, 0, 1)  
-color2 = (1, 0, 0)   
+# نقاط و رنگ‌ها
+point1 = (200,100 )  # نقطه اول
+point2 = (100, 300)  # نقطه دوم
+color1 = (0, 0, 1)   # رنگ آبی (RGB)
+color2 = (1, 0, 0)   # رنگ قرمز (RGB)
 
 image = draw_gradient(point1, point2, color1, color2)
-
-plt.imshow(image)
-plt.axis('off') 
-plt.show()
+cv2.imshow('image',image)
+cv2.waitKey(0)
